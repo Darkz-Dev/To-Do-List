a30b347c519938f0db7a39c7b2b6647070c6a218
Darkz-Dev <yurissantos.dev@gmail.com>
2026-07-18 19:54:46 -0300

    novos id no index para as ul que eu tinha esquecido e começo do meu trabalho no javascript

---
+++ List.js
@@ -0,0 +1,8 @@
+const falta = document.getElementById('falta')
+const addTarefa = document.querySelector('.addTarefa')
+const novaTarefa = document.querySelector('.novaTarefa')
+const listaFalta = document.getElementById('listaFalta')
+const listaFazendo = document.getElementById('listaFazendo')
+const listaFeita = document.getElementById('listaFeita')
+
+
