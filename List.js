bcd41e4eaa2ac236d55ded745404360c8182f2f1
Darkz-Dev <yurissantos.dev@gmail.com>
2026-07-19 16:31:56 -0300

    **Criação dinâmica dos itens da lista via JavaScript**
    * Implementada a criação dos elementos `<li>` e `<button>` dinamicamente com JavaScript.
    * Adicionada a funcionalidade do botão **Adicionar**, inserindo novas tarefas na lista **Falta**.
    * Implementada a funcionalidade de remoção, permitindo que cada botão exclua apenas o `<li>` ao qual pertence, sem afetar os demais itens.
    * Utilizado o método `trim()` para evitar o cadastro de tarefas contendo apenas espaços em branco ou espaços desnecessários no início e no fim do texto.
    * Criada uma classe CSS para estilizar o botão de remoção.
    * Aprendido e aplicado o uso de `classList.add()` para adicionar classes CSS aos elementos criados dinamicamente via JavaScript.

--- List.js
+++ List.js
@@ -1,3 +1,5 @@
+//Tem muitos comentarios para eu não esquecer oq cada linha faz
+
 const falta = document.getElementById('falta')
 const addTarefa = document.querySelector('.addTarefa')
 const novaTarefa = document.querySelector('.novaTarefa')
@@ -5,4 +7,37 @@ const listaFalta = document.getElementById('listaFalta')
 const listaFazendo = document.getElementById('listaFazendo')
 const listaFeita = document.getElementById('listaFeita')
 
+    function addItem(texto){
+        
+        let item = document.createElement("li")
+
+        item.textContent = texto
+
+        let botao = document.createElement("button")
+
+        botao.textContent = "X"
+        
+        botao.classList.add("btn-apagar"); //Justamente essa parte que eu nao sabia como puxar de la
+
+        botao.addEventListener("click",(event) => {
+            
+            item.remove()
+        })
+
+        item.appendChild(botao)
+
+        listaFalta.appendChild(item)
+
+        
+        
+    }
+
+    addTarefa.addEventListener('click', () => {
+    let tarefa = novaTarefa.value.trim(); // Pega o que foi digitado
+    
+    if (tarefa !== "") {
+        addItem(tarefa); // Chama a sua função passando o texto
+        novaTarefa.value = ""; // Limpa o campo
+    }
+})
 
